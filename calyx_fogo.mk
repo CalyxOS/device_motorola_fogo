db2ca9242e7161ecdff20bc7f46858bbed8de2c0
Michael Bestas <mkbestas@gmail.com>
2023-04-04 22:10:12 +0300

    Initial CalyxOS makefile
    Change-Id: I6dfcc9b2b5b686df8d2bbe4183bb687777ab576e

---
+++ calyx_fogo.mk
@@ -0,0 +1,26 @@
+#
+# SPDX-FileCopyrightText: The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common stuff
+$(call inherit-product, vendor/calyx/config/common_phone.mk)
+
+# Inherit from fogo device
+$(call inherit-product, device/motorola/fogo/device.mk)
+
+PRODUCT_NAME := calyx_fogo
+PRODUCT_DEVICE := fogo
+PRODUCT_MANUFACTURER := motorola
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := moto g 5G - 2024
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    BuildDesc="fogo_g-user 15 V1UFNS35H.193-20-14 2c2db8-05b2f release-keys MV-304" \
+    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFNS35H.193-20-14/2c2db8-05b2f:user/release-keys \
+    DeviceProduct=fogo_g
