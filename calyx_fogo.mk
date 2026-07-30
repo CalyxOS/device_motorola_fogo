427a6f5f2ee5822a3806ca953fd0a347fc37efe4
Michael Bestas <mkbestas@gmail.com>
2026-07-30 17:51:36 +0300

    calyx: Update to V1UFNS35H.193-20-14
    Change-Id: I2eff280ec2a01a4077b346740ad2cfdea0beedf4

--- calyx_fogo.mk
+++ calyx_fogo.mk
@@ -21,6 +21,6 @@ PRODUCT_BRAND := motorola
 PRODUCT_MODEL := moto g 5G - 2024
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    BuildDesc="fogo_g-user 15 V1UFNS35H.193-20-12 66040b-a80b154 release-keys MV-304" \
-    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFNS35H.193-20-12/66040b-a80b154:user/release-keys \
+    BuildDesc="fogo_g-user 15 V1UFNS35H.193-20-14 2c2db8-05b2f release-keys MV-304" \
+    BuildFingerprint=motorola/fogo_g/fogo:15/V1UFNS35H.193-20-14/2c2db8-05b2f:user/release-keys \
     DeviceProduct=fogo_g
